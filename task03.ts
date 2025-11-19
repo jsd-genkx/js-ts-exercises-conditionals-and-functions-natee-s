b64d59a7f67644a47203a4acf79887d1a9081040
natee-s <environmental_man@hotmail.com>
2025-11-19 23:36:04 +0700

    add:solution task03 and task04

--- task03.ts
+++ task03.ts
@@ -2,5 +2,25 @@
 // Password < 6 chars → "Too short"
 // 6–10 chars → "Moderate"
 // 11+ chars → "Strong"
+// console.log(password.length);
 
-const password = "pass123";
+const password = "pass1234";
+
+if( password.length < 6 ){
+    console.log(`the password are ${password.length} it is "Too short"`)
+}
+else if(password.length >= 6 && password.length <= 10 ){
+    console.log(`the password are ${password.length} it is "Moderate"`)
+}
+else{
+    console.log(`the password are ${password.length} it is "Strong"`)
+}
+
+
+// if (password.length < 6) {
+//     console.log("Too short");
+// } else if (password.length <= 10) { // ไม่ต้องเช็ค >= 6 เพราะถ้าไม่เข้าเงื่อนไขแรก (if) มันก็ต้อง >= 6 อยู่แล้ว
+//     console.log("Moderate");
+// } else { // ส่วนนี้คือส่วนที่ไม่เข้าเงื่อนไขข้างบนสักข้อ ซึ่งหมายถึงความยาว 11+ โดยอัตโนมัติ
+//     console.log("Strong");
+// }
--- task04.ts
+++ task04.ts
@@ -1,9 +1,35 @@
+const num = 18;
 // TODO-04: Write if/else if to check and print:
 // Divisible by both 3 and 5 → "FizzBuzz"
 // Divisible by only 3 → "Fizz"
 // Divisible by only 5 → "Buzz"
 // Otherwise → "Not divisible by 3 or 5"
 
+// if( num % 3 === 0 && num % 5 === 0 ){
+//     console.log("FizzBuzz")
+// } else if(num % 3 === 0){
+//     console.log("Fizz")
+// } else if(num % 5 === 0){
+//     console.log("Buzz")
+// } else{
+//     console.log("Not divisible by 3 or 5")
+// }
+
 // TODO BONUS: Rewrite with if
 
-const num = 15;
+if (num % 3 === 0) {               // เช็คภายในว่า หารด้วย 5 ลงตัวด้วยไหม
+  if (num % 5 === 0) {             // ถ้าข้างบนจริง เช็คภายในว่า หารด้วย 5 ลงตัวด้วยไหม
+    console.log("FizzBuzz");       // ถ้าหารทั้ง 3 และ 5 ลงตัว ให้แสดง "FizzBuzz"
+  } else {
+    console.log("Fizz");            // ให้แสดง"Fizz" ถ้าหารลงตัวเฉพาะ 3
+  }
+} 
+
+if (num % 3 !== 0) {                // เข้ามาเฉพาะเมื่อไม่ใช่กรณีที่หารด้วย 3
+  if (num % 5 === 0) {              // เช็คภายในว่า หาร 5 ลงตัวด้วยไหม
+    console.log("Buzz");            // ถ้าหารเฉพาะ 5 ลงตัว ให้แสดง Buzz
+  } else {
+    console.log("Not divisible by 3 or 5");   // ถ้าหารไม่ลงตัวทั้งคู่
+  }
+}
+
