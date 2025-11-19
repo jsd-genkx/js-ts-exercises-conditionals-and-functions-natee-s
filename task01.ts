190eada8d6cd01730bb253d2b281d1e82404f862
natee-s <environmental_man@hotmail.com>
2025-11-19 15:49:31 +0700

    add:solution of task02

--- task01.ts
+++ task01.ts
@@ -10,3 +10,13 @@ if(a === 0 ){
 }
 
 
+
+// TODO-01: Use if/else to print "Even" or "Odd"
+// const number = 15;
+// % คือ modulo operator → หารเอาเศษ
+// if ( number % 2 === 0 ) {
+//     console.log("Even");
+// }
+// else
+//     {console.log("Odd");
+// }
--- task02.ts
+++ task02.ts
@@ -1,11 +1,21 @@
 // TODO-02: Use if/else to check if it's a number
 // (hint: typeof or isNaN)
 
-const userInput = "42";
-let a = typeof userInput
+const userInput = 42;
+// let a = typeofuserInput
 // Try changing to "hello", 10, null, NaN
-if(a = number){
-    console.log("is number", yes);
+if(typeof userInput === "number" ){
+    console.log("Yes is number");
 } else{
-    console.log("is number", No);
+    console.log("No is not number");
 }
+
+// // TODO-02: Use if/else to check if it's a number
+// // (hint: typeof or isNaN)
+// const userInput = 42;
+// // Try changing to "hello", 10, null, NaN
+// if (typeof userInput === 'number' && !isNaN(userInput)){
+//     console.log(`T :${userInput} <- This is Number`);
+// }else{
+//     console.log(`F :${userInput} <-This is not number /is NaN`);
+// }
