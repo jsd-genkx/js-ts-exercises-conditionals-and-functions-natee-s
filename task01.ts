24c11d7e7c920c529fa4a5f86dc0379ef4a2e4db
natee-s <environmental_man@hotmail.com>
2025-11-19 15:20:13 +0700

    add:solution of task01

--- task01.ts
+++ task01.ts
@@ -1,3 +1,12 @@
 // TODO-01: Use if/else to print "Even" or "Odd"
 
 const number = 15;
+let a = number%2;
+
+if(a === 0 ){
+    console.log("Even");
+} else{
+    console.log("Odd");
+}
+
+
--- task02.ts
+++ task02.ts
@@ -2,4 +2,10 @@
 // (hint: typeof or isNaN)
 
 const userInput = "42";
+let a = typeof userInput
 // Try changing to "hello", 10, null, NaN
+if(a = number){
+    console.log("is number", yes);
+} else{
+    console.log("is number", No);
+}
