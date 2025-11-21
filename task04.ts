733e96bb5a93ec27ced965c179c7d5abef19e8ee
natee-s <environmental_man@hotmail.com>
2025-11-21 14:12:54 +0700

    add:function_fizz_buzz

--- task04.ts
+++ task04.ts
@@ -33,15 +33,38 @@
 //   }
 // }
 
-let num = 0;
-if(num === 0){
-  console.log("Zero is not acceptable.")
-} else if ((num % 3 === 0) && (num % 5 === 0)) {
-  console.log(`Your number:${num} is "FizzBuzz".`);
-} else if ((num % 3 === 0) ) {
-    console.log(`Your number:${num} is "Fizz".`);
-} else if ((num % 5 === 0) ) {
-    console.log(`Your number:${num} is "Buzz".`);
-} else {
-  console.log("Not divisible by 3 or 5");
+const num = 15;
+// if(num === 0){
+//   console.log("Zero is not acceptable.")
+// } else if ((num % 3 === 0) && (num % 5 === 0)) {
+//   console.log(`Your number:${num} is "FizzBuzz".`);
+// } else if ((num % 3 === 0) ) {
+//     console.log(`Your number:${num} is "Fizz".`);
+// } else if ((num % 5 === 0) ) {
+//     console.log(`Your number:${num} is "Buzz".`);
+// } else {
+//   console.log("Not divisible by 3 or 5");
+// }
+
+const fizzBuzz = (number:number) => {
+if( number % 3 === 0 && number % 5 === 0 ){
+    console.log("FizzBuzz")
+} else if(number % 3 === 0){
+    console.log("Fizz")
+} else if(number % 5 === 0){
+    console.log("Buzz")
+} else{
+    console.log("Not divisible by 3 or 5")
 }
+}
+
+fizzBuzz(5)
+const fizzBuzz2 = fizzBuzz
+fizzBuzz2(7)
+fizzBuzz(3)
+
+// Example Test Cases
+fizzBuzz(15); // "FizzBuzz"
+fizzBuzz(9); // "Fizz"
+fizzBuzz(10); // "Buzz"
+fizzBuzz(7); // "Not divisible by 3 or 5"
